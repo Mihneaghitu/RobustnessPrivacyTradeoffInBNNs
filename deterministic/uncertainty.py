Fix dp mechanism (clipping before batch averaging gradients) and add partially completed experiments setup

deterministic/uncertainty.py
@@ -36,9 +36,9 @@ def auroc(net: VanillaNetLinear, test_set: Dataset) -> float:
     assert np.isclose(auc_val.item(), alt_auroc, atol=1e-5)
     # ----------------------------------------------------------
 
-    return auc_val
+    return round(float(auc_val.item()), 4)
 
-def ece(net: VanillaNetLinear, test_set: Dataset):
+def ece(net: VanillaNetLinear, test_set: Dataset) -> float:
     net.eval()
     data_loader = DataLoader(test_set, batch_size=1000, shuffle=False)
     y_pred = torch.tensor([], dtype=torch.float32, device=TORCH_DEVICE)
@@ -50,4 +50,4 @@ def ece(net: VanillaNetLinear, test_set: Dataset):
     mc_ece = MulticlassCalibrationError(num_classes=10, n_bins=10, norm='l1')
     ece_val = mc_ece(y_pred, copy.deepcopy(test_set.targets).to(TORCH_DEVICE))
 
-    return ece_val
+    return round(float(ece_val.item()), 4)

main.py
@@ -1,28 +1,23 @@
 import torch
+import yaml
 
 from common.attack_types import AttackType
 from common.dataset_utils import get_marginal_distributions, load_mnist
 from common.datasets import GenericDataset
-from deterministic.attacks import \
-    fgsm_test_set_attack as fgsm_test_set_attack_dnn
-from deterministic.attacks import ibp_eval as ibp_eval_dnn
-from deterministic.attacks import \
-    pgd_test_set_attack as pgd_test_set_attack_dnn
 from deterministic.hyperparams import Hyperparameters
 from deterministic.membership_inference_dnn import MembershipInferenceAttack
 from deterministic.pipeline import PipelineDnn
-from deterministic.uncertainty import auroc as auroc_dnn
-from deterministic.uncertainty import ece as ece_dnn
 from deterministic.vanilla_net import VanillaNetLinear
+from experiments.experiment_utils import (compute_metrics_hmc,
+                                          compute_metrics_sgd,
+                                          run_experiment_adv_hmc,
+                                          run_experiment_hmc,
+                                          run_experiment_sgd)
 from globals import TORCH_DEVICE
-from probabilistic.HMC.adv_robust_dp_hmc import AdvHamiltonianMonteCarlo
-from probabilistic.HMC.attacks import (fgsm_predictive_distrib_attack,
-                                       ibp_eval, pgd_predictive_distrib_attack)
 from probabilistic.HMC.hmc import HamiltonianMonteCarlo
 from probabilistic.HMC.hyperparams import HyperparamsHMC
 from probabilistic.HMC.membership_inference_bnn import \
     MembershipInferenceAttackBnn
-from probabilistic.HMC.uncertainty import auroc, ece, ibp_auc_and_ece
 from probabilistic.HMC.vanilla_bnn import VanillaBnnMnist
 
 
@@ -30,79 +25,64 @@ def main():
     #* Seed is set in globals.py
     # membership_inference_dnn_experiment()
     # membership_inference_bnn_experiment()
-    # adv_dp_experiment()
-    hmc_dp_experiment()
-    # dnn_experiment(save_model=False)
+    adv_dp_experiment(write_results=True, save_model=True)
+    # hmc_dp_experiment(write_results=True)
+    # dnn_experiment(save_model=False, write_results=False)
     return 0
 
-def adv_dp_experiment():
+def adv_dp_experiment(write_results: bool = False, save_model: bool = False):
     print(f"Using device: {TORCH_DEVICE}")
-    # vanilla_bnn = VanillaBnnMnist().to(TORCH_DEVICE)
     vanilla_bnn = VanillaBnnMnist().to(TORCH_DEVICE)
     train_data, test_data = load_mnist()
-    hyperparams_1 = HyperparamsHMC(num_epochs=20, num_burnin_epochs=20, step_size=1e-4, warmup_step_size=0.2, lf_steps=120,
-                                   batch_size=500, momentum_std=0.01, alpha=0.25, eps=0.1, decay_epoch_start=15, lr_decay_magnitude=0.1,
-                                   eps_warmup_epochs=0, alpha_warmup_epochs=0, run_dp=False, grad_clip_bound=0.05,
-                                   acceptance_clip_bound=0.01, tau_g=0.1, tau_l=0.1)
+    hyperparams = HyperparamsHMC(num_epochs=70, num_burnin_epochs=0, step_size=4e-3, warmup_step_size=0.75, lf_steps=120,
+                                 batch_size=500, momentum_std=0.002, alpha=0.4, eps=0.1, decay_epoch_start=20,
+                                 lr_decay_magnitude=0.1, eps_warmup_epochs=0, alpha_warmup_epochs=0, run_dp=False,
+                                 grad_clip_bound=0.25, acceptance_clip_bound=0.25, tau_g=0.5, tau_l=0.5)
 
-    hmc = AdvHamiltonianMonteCarlo(vanilla_bnn, hyperparams_1, attack_type=AttackType.IBP)
+    attack_type = AttackType.IBP
+    model_name = "ADV-DP-HMC" if hyperparams.run_dp else "ADV-HMC"
+    fname = "hmc_mnist_dp" if hyperparams.run_dp else "hmc_mnist"
+    if attack_type == AttackType.FGSM:
+        model_name += " (FGSM)"
+        fname = "fgsm_" + fname
+    elif attack_type == AttackType.PGD:
+        model_name += " (PGD)"
+        fname = "pgd_" + fname
+    else:
+        model_name += " (IBP)"
+        fname = "ibp_" + fname
+    fname = fname if save_model else None
 
-    samples = hmc.train_with_restarts(train_data, first_chain_from_trained=True)
+    hmc, posterior_samples = run_experiment_adv_hmc(vanilla_bnn, train_data, hyperparams, attack_type, fname, init_from_trained=True)
+    compute_metrics_hmc(hmc ,test_data, posterior_samples, testing_eps=0.075, write_results=write_results,
+                        model_name=model_name, dset_name="MNIST")
 
-    hmc.hps.eps = 0.0675
-    adv_test_set_pgd = pgd_predictive_distrib_attack(hmc.net, hmc.hps, test_data, samples)
-    adv_test_set_fgsm = fgsm_predictive_distrib_attack(hmc.net, hmc.hps, test_data, samples)
 
-    print('------------------- Normal Accuracy -------------------')
-    acc_standard = hmc.test_hmc_with_average_logits(test_data, samples)
-    print(f'Accuracy of ADV-HMC-DP with average logit on standard test set: {acc_standard} %')
-    print('-------------------------------------------------------')
-
-    print('------------------- FGSM attack --------------------')
-    acc_fgsm = hmc.test_hmc_with_average_logits(adv_test_set_fgsm, samples)
-    print(f'Accuracy of ADV-HMC-DP with average logit on FGSM adversarial test set: {acc_fgsm} %')
-    print('----------------------------------------------------')
-
-    print('------------------- PGD attack -------------------')
-    acc_pgd = hmc.test_hmc_with_average_logits(adv_test_set_pgd, samples)
-    print(f'Accuracy of ADV-HMC-DP with average logit on PGD adversarial test set: {acc_pgd} %')
-    print('---------------------------------------------------')
-
-    print('------------------- IBP attack -------------------')
-    acc_ibp = ibp_eval(hmc.net, hmc.hps, test_data, samples)
-    print(f'Accuracy of ADV-HMC-DP with IBP on standard test set: {acc_ibp} %')
-    print('---------------------------------------------------')
-
-    print('------------------- Uncertainty metrics -------------------')
-    auc_val = auroc(hmc.net, test_data, samples)
-    print(f'STD AUC: {auc_val}')
-    ece_val = ece(hmc.net, test_data, samples)
-    print(f'STD ECE: {ece_val}')
-    fgsm_auc_val = auroc(hmc.net, adv_test_set_fgsm, samples)
-    print(f'FGSM AUC: {fgsm_auc_val}')
-    fgsm_ece_val = ece(hmc.net, adv_test_set_fgsm, samples)
-    print(f'FGSM ECE: {fgsm_ece_val}')
-    pgd_auc_val = auroc(hmc.net, adv_test_set_pgd, samples)
-    print(f'PGD AUC: {pgd_auc_val}')
-    pgd_ece_val = ece(hmc.net, adv_test_set_pgd, samples)
-    print(f'PGD ECE: {pgd_ece_val}')
-    ibp_auc_val, ibp_ece_val = ibp_auc_and_ece(hmc.net, test_data, samples, hmc.hps.eps)
-    print(f'IBP AUC: {ibp_auc_val}')
-    print(f'IBP ECE: {ibp_ece_val}')
-    print('-----------------------------------------------------------')
-
-def hmc_dp_experiment():
+def hmc_dp_experiment(write_results: bool = False, save_model: bool = False):
     train_data, test_data = load_mnist()
     print("Training Bayesian Neural Network using HMC...")
     target_network = VanillaBnnMnist().to(TORCH_DEVICE)
-    hyperparams = HyperparamsHMC(num_epochs=120, num_burnin_epochs=40, step_size=0.005, lf_steps=100,
-                                 batch_size=512, momentum_std=0.01, decay_epoch_start=40, lr_decay_magnitude=0.1,
-                                 run_dp=True, grad_clip_bound=0.3, acceptance_clip_bound=0.3, tau_g=0.5, tau_l=0.5)
-    hmc = HamiltonianMonteCarlo(target_network, hyperparams)
-    posterior_samples = hmc.train_bnn(train_data)
-    accuracy = hmc.test_hmc_with_average_logits(test_data, posterior_samples)
-    print(f"Accuracy of BNN with average logits on training set: {accuracy} %")
+    hyperparams = HyperparamsHMC(num_epochs=100, num_burnin_epochs=25, step_size=0.02, lf_steps=100,
+                                 batch_size=512, momentum_std=0.01, decay_epoch_start=25, lr_decay_magnitude=0.05,
+                                 run_dp=False, grad_clip_bound=2, acceptance_clip_bound=0.5, tau_g=0.5, tau_l=0.5)
 
+    fname = f'''hmc_mnist{"_dp" if hyperparams.run_dp else ""}.pt''' if save_model else None
+    hmc, posterior_samples = run_experiment_hmc(target_network, train_data, hyperparams, save_file_name=fname)
+    model_name = "HMC-DP" if hyperparams.run_dp else "HMC"
+    compute_metrics_hmc(hmc ,test_data, posterior_samples, testing_eps=0.075, write_results=write_results,
+                        model_name=model_name, dset_name="MNIST")
+
+def dnn_experiment(save_model: bool = False, write_results: bool = False):
+    train, test = load_mnist()
+    net = VanillaNetLinear().to(TORCH_DEVICE)
+    hyperparams = Hyperparameters(num_epochs=12, lr=0.01, batch_size=60, lr_decay_magnitude=0.1, decay_epoch_start=15,
+                                  alpha=0.5, eps=0.1, eps_warmup_itrs=4000, alpha_warmup_itrs=4000, warmup_itr_start=3000)
+
+    fname = "vanilla_network.pt" if save_model else None
+    pipeline = run_experiment_sgd(net, train, hyperparams, AttackType.IBP, save_file_name=fname)
+    torch.save(pipeline.net.state_dict(), "vanilla_network.pt")
+
+    compute_metrics_sgd(pipeline, test, testing_eps=0.075, write_results=write_results, dset_name="MNIST")
 
 def membership_inference_dnn_experiment():
     # -------------- Hyperparams Values --------------
@@ -180,40 +160,6 @@ def membership_inference_bnn_experiment():
     print("Testing attack models...")
     membership_inference_attack.test_attack_models_dnn(attack_models_dset)
     print("Finished testing attack models.")
-
-def dnn_experiment(test_only: bool = False, save_model: bool = False):
-    net = VanillaNetLinear().to(TORCH_DEVICE)
-    curr_dir = __file__.rsplit('/', 2)[0]
-    hyperparams = Hyperparameters(num_epochs=120, lr=0.01, batch_size=60, lr_decay_magnitude=0.005, decay_epoch_start=35,
-                                  alpha=1, eps=0.1, eps_warmup_itrs=10000, alpha_warmup_itrs=10000, warmup_itr_start=5000)
-
-    pipeline = PipelineDnn(net, hyperparams, AttackType.IBP)
-    if not test_only:
-        train, test = load_mnist()
-        pipeline.train_mnist_vanilla(train)
-        if save_model:
-            torch.save(pipeline.net.state_dict(), curr_dir + '/vanilla_network.pt')
-    else:
-        print("Testing only...")
-        net = VanillaNetLinear()
-        net.load_state_dict(torch.load(curr_dir + 'vanilla_network.pt'))
-
-    hyperparams.eps = 0.05
-
-    std_acc = pipeline.test_mnist_vanilla(test)
-    print(f'Standard accuracy of the network on the 10000 test images: {std_acc}%')
-    fgsm_test_set = fgsm_test_set_attack_dnn(net, hyperparams, test)
-    fgsm_acc = pipeline.test_mnist_vanilla(fgsm_test_set)
-    print(f'Accuracy of the network using FGSM adversarial test set: {fgsm_acc}%')
-    pgd_test_set = pgd_test_set_attack_dnn(net, hyperparams, test)
-    pgd_acc = pipeline.test_mnist_vanilla(pgd_test_set)
-    print(f'Accuracy of the network using PGD adversarial test set: {pgd_acc}%')
-    ibp_acc = ibp_eval_dnn(net, hyperparams, test)
-    print(f'Accuracy of the network using IBP adversarial test set: {ibp_acc}%')
-    auc_val = auroc_dnn(net, test)
-    ece_val = ece_dnn(net, test)
-    print(f'AUC: {auc_val}')
-    print(f'ECE: {ece_val}')
 
 if __name__ == '__main__':
     main()

probabilistic/HMC/adv_robust_dp_hmc.py
@@ -12,6 +12,8 @@ from torch.utils.data import DataLoader
 
 sys.path.append('../../')
 
+from torch.func import functional_call, grad, vmap
+
 from common.attack_types import AttackType
 from globals import LOGGER_TYPE, TORCH_DEVICE, LoggerType
 from probabilistic.HMC.hyperparams import HyperparamsHMC
@@ -114,7 +116,7 @@ class AdvHamiltonianMonteCarlo:
                 end_energy = self.__get_energy(q, p, data_loader)
                 acceptance_prob = min(1, torch.exp(end_energy - initial_energy))
             else:
-                acceptance_prob = self.__get_dp_energy(q, p, current_q, current_p, data_loader)
+                acceptance_prob = min(1, self.__get_dp_energy(q, p, current_q, current_p, data_loader))
 
             print(f'Acceptance probability: {acceptance_prob}')
             if LOGGER_TYPE == LoggerType.WANDB:
@@ -182,7 +184,7 @@ class AdvHamiltonianMonteCarlo:
 
         return closs, closs_adv
 
-    def __advance_momentum(self, p: list, eps: float, adv=False) -> None:
+    def __advance_momentum(self, p: list, lr: float, adv=False) -> None:
         prior_loss = torch.tensor(0.0).requires_grad_(True).to(TORCH_DEVICE)
         prior_grad = torch.tensor(0.0).requires_grad_(True).to(TORCH_DEVICE)
         for idx, param in enumerate(self.net.parameters()):
@@ -190,8 +192,12 @@ class AdvHamiltonianMonteCarlo:
             if not adv:
                 prior_loss = prior_loss + torch.neg(torch.mean(dist.Normal(self.hps.prior_mu, self.hps.prior_std).log_prob(param)))
                 prior_grad = torch.autograd.grad(outputs=prior_loss, inputs=param)[0]
+                prior_grad /= self.hps.alpha
+            if self.hps.run_dp:
+                total_batch_noise = dist.Normal(0, 2 * self.hps.tau_g * self.hps.grad_clip_bound).sample(param.shape).to(TORCH_DEVICE)
+                potential_energy_grad += total_batch_noise / self.hps.batch_size
             potential_energy_grad = self.__get_dp_grads(ll_grad) + prior_grad
-            p[idx] = self.net.update_param(p[idx], potential_energy_grad, eps)
+            p[idx] = self.net.update_param(p[idx], potential_energy_grad, lr)
 
         self.net.zero_grad()
 
@@ -214,7 +220,25 @@ class AdvHamiltonianMonteCarlo:
             y_hat = self.net(batch_data)
             closs = criterion(y_hat, batch_target)
 
-        closs.backward()
+        self.net.zero_grad()
+        if self.hps.run_dp:
+            params = {k: v.detach() for k, v in self.net.named_parameters()}
+            buffers = {k: v.detach() for k, v in self.net.named_buffers()}
+            compute_grad = grad(self.__compute_per_sample_loss)
+            compute_per_sample_grads = vmap(compute_grad, in_dims=(None, None, 0, 0))
+            per_sample_grads = compute_per_sample_grads(params, buffers, batch_data, batch_target)
+
+            per_layer_mean_clipped_grads = []
+            for batch_grads in per_sample_grads.values():
+                clip_per_sample_grad = vmap(self.__clip, in_dims=(0, None), out_dims=0)
+                clipped_per_sample_grads = clip_per_sample_grad(batch_grads, self.hps.grad_clip_bound)
+                per_layer_mean_clipped_grads.append(torch.mean(clipped_per_sample_grads, dim=0)) # average over samples
+            for idx, param in enumerate(self.net.parameters()):
+                param.grad = per_layer_mean_clipped_grads[idx]
+            # Now we are exactly in the same situation as in the non-DP case, i.e. with the mean batch gradients inside the
+            # parameters' grad attributes, with the exception that everything was clipped to ensure privacy
+        else:
+            closs.backward()
 
         return closs
 
@@ -282,22 +306,25 @@ class AdvHamiltonianMonteCarlo:
         return potential_energy + kinetic_energy
 
     def __get_dp_energy(self, prop_q: list, prop_p: list, curr_q: list, curr_p: list, data_loader: torch.utils.data.DataLoader) -> torch.Tensor:
+        compute_per_sample_loss = vmap(self.hps.criterion, in_dims=(0, 0), out_dims=0)
         # save the current parameters
         start_params = self.net.get_params()
 
         batch_data, batch_target = self.__get_batch(data_loader)
         self.net.set_params(curr_q)
-        nll_initial = self.hps.criterion(self.net(batch_data), batch_target)
+        initial_per_sample_nll = compute_per_sample_loss(self.net(batch_data), batch_target)
         self.net.set_params(prop_q)
-        nll_proposal = self.hps.criterion(self.net(batch_data), batch_target)
-        ll_ratio_batch = nll_proposal - nll_initial # log of ratio of likelihoods: ln(p(x | theta') / p(x | theta))
+        proposal_per_sample_nll = compute_per_sample_loss(self.net(batch_data), batch_target)
 
-        # clip the log likelihood ratio for privacy in the acceptance step
+        # get the norm of the difference between the current and the proposal parameters, needed for the clip bound
         diff = torch.tensor([]).to(TORCH_DEVICE)
         for c_q, p_q in zip(curr_q, prop_q):
             diff = torch.cat((diff, torch.flatten(p_q - c_q)))
-        ll_ratio_clipped_bound = self.hps.acceptance_clip_bound * torch.norm(diff)
-        clipped_ll_ratio_batch = self.__clip(ll_ratio_batch, ll_ratio_clipped_bound)
+
+        #* compute the clipping bound, which is ||theta_prop - theta_curr||_2 * b_l, then use a vmap to clip the ratio for every sample
+        clipping_bound_ll_ratio = self.hps.acceptance_clip_bound * torch.norm(diff)
+        per_sample_clipped_ll_ratio = vmap(lambda nom, denom: self.__clip(nom / denom, clipping_bound_ll_ratio), in_dims=(0, 0), out_dims=0)
+        clipped_ll_ratio_batch = torch.mean(per_sample_clipped_ll_ratio(proposal_per_sample_nll, initial_per_sample_nll))
 
         # compute the kinetic energy
         kinetic_energy_init = torch.tensor(0.0).to(TORCH_DEVICE)
@@ -315,7 +342,7 @@ class AdvHamiltonianMonteCarlo:
         prior_ratio_batch = proposal_prior_prob - init_prior_prob
 
         # noise term
-        dp_sigma_acceptance = 2 * self.hps.tau_l * ll_ratio_clipped_bound
+        dp_sigma_acceptance = 2 * self.hps.tau_l * clipping_bound_ll_ratio
         psi = dist.Normal(0, dp_sigma_acceptance).sample().to(TORCH_DEVICE)
 
         # correction term
@@ -342,6 +369,14 @@ class AdvHamiltonianMonteCarlo:
     def __clip(self, x: torch.Tensor, bound: float) -> torch.Tensor:
         return x * min(1, bound / torch.norm(x))
 
+    def __compute_per_sample_loss(self, params: torch.Tensor, buffers: torch.Tensor, sample: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+        batch = sample.unsqueeze(0)
+        targets = target.unsqueeze(0)
+
+        predictions = functional_call(self.net, (params, buffers), (batch,))
+        loss = self.hps.criterion(predictions, targets)
+        return loss
+
 
     def __get_batch(self, data_loader: torch.utils.data.DataLoader) -> Tuple[torch.Tensor, torch.Tensor]:
         data = next(iter(data_loader))
@@ -350,11 +385,15 @@ class AdvHamiltonianMonteCarlo:
         return batch_data, batch_target
 
     def __run_schedule(self,curr_epoch: int) -> None:
+        decay_step = self.init_hps.step_size * (1 - self.hps.lr_decay_magnitude) / (self.hps.num_epochs - self.hps.decay_epoch_start)
+        if curr_epoch > self.hps.decay_epoch_start and self.hps.step_size > self.init_hps.step_size * self.hps.lr_decay_magnitude:
+            self.hps.step_size -= decay_step
+
         if self.attack_type in [AttackType.FGSM, AttackType.PGD] or self.hps.num_burnin_epochs == 0:
-            self.hps.eps, self.hps.alpha = self.init_hps.eps, self.init_hps.alpha
-            self.hps.step_size = self.init_hps.step_size if curr_epoch > self.hps.num_burnin_epochs else self.init_hps.warmup_step_size
+            self.hps.eps, self.hps.alpha, self.hps.step_size = self.init_hps.eps, self.init_hps.alpha, self.init_hps.step_size
             return
 
+        # Only for IBP
         delta_eps, delta_alpha = self.init_hps.eps, 1 - self.init_hps.alpha
         increment_eps, increment_alpha = delta_eps / self.hps.eps_warmup_epochs, delta_alpha / self.hps.alpha_warmup_epochs
 
@@ -366,10 +405,6 @@ class AdvHamiltonianMonteCarlo:
             self.hps.step_size = self.init_hps.warmup_step_size
         else:
             self.hps.step_size = self.init_hps.step_size
-
-        decay_step = self.init_hps.step_size * (1 - self.hps.lr_decay_magnitude) / (self.hps.num_epochs - self.hps.decay_epoch_start)
-        if curr_epoch > self.hps.decay_epoch_start and self.hps.step_size > self.init_hps.step_size * self.hps.lr_decay_magnitude:
-            self.hps.step_size -= decay_step
 
     def __init_params(self, from_trained: bool = False, path: str = None) -> List[torch.Tensor]:
         init_q = []

probabilistic/HMC/hmc.py
@@ -7,6 +7,7 @@ import torch.distributions as dist
 import torch.nn.functional as F
 import torchvision
 import wandb
+from torch.func import functional_call, grad, vmap
 from torch.utils.data import DataLoader
 
 sys.path.append('../../')
@@ -20,7 +21,6 @@ class HamiltonianMonteCarlo:
     def __init__(self, net: VanillaBnnLinear, hyperparameters: HyperparamsHMC) -> None:
         self.net = net
         self.hps = hyperparameters
-
 
     def train_bnn(self, train_set: torchvision.datasets.mnist) -> List[torch.tensor]:
         print_freq = self.hps.lf_steps - 1
@@ -105,7 +105,7 @@ class HamiltonianMonteCarlo:
             self.net.set_params(sample)
             self.net.eval()
             batch_size = 32
-            data_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=2)
+            data_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
 
             sample_results = torch.tensor([]).to(TORCH_DEVICE)
             for data, _ in data_loader:
@@ -114,7 +114,6 @@ class HamiltonianMonteCarlo:
                 sample_results = torch.cat((sample_results, y_hat), dim=0)
             average_logits += sample_results / len(posterior_samples)
 
-        print(f"Len posterior samples: {len(posterior_samples)}")
         correct, total = 0, test_set.targets.size(0)
         for i in range(test_set.targets.size(0)):
             avg_logit = average_logits[i]
@@ -145,7 +144,10 @@ class HamiltonianMonteCarlo:
             ll_grad = param.grad
             prior_loss = prior_loss + torch.neg(torch.mean(dist.Normal(self.hps.prior_mu, self.hps.prior_std).log_prob(param)))
             prior_grad = torch.autograd.grad(outputs=prior_loss, inputs=param)[0]
-            potential_energy_grad = self.__get_dp_grads(ll_grad) + prior_grad
+            potential_energy_grad = ll_grad + prior_grad
+            if self.hps.run_dp:
+                total_batch_noise = dist.Normal(0, 2 * self.hps.tau_g * self.hps.grad_clip_bound).sample(param.shape).to(TORCH_DEVICE)
+                potential_energy_grad += total_batch_noise / self.hps.batch_size
             p[idx] = self.net.update_param(p[idx], potential_energy_grad, eps)
 
         self.net.zero_grad()
@@ -162,7 +164,24 @@ class HamiltonianMonteCarlo:
         y_hat = self.net(batch_data)
         closs = criterion(y_hat, batch_target)
         self.net.zero_grad()
-        closs.backward()
+        if self.hps.run_dp:
+            params = {k: v.detach() for k, v in self.net.named_parameters()}
+            buffers = {k: v.detach() for k, v in self.net.named_buffers()}
+            compute_grad = grad(self.__compute_per_sample_loss)
+            compute_per_sample_grads = vmap(compute_grad, in_dims=(None, None, 0, 0))
+            per_sample_grads = compute_per_sample_grads(params, buffers, batch_data, batch_target)
+
+            per_layer_mean_clipped_grads = []
+            for batch_grads in per_sample_grads.values():
+                clip_per_sample_grad = vmap(self.__clip, in_dims=(0, None), out_dims=0)
+                clipped_per_sample_grads = clip_per_sample_grad(batch_grads, self.hps.grad_clip_bound)
+                per_layer_mean_clipped_grads.append(torch.mean(clipped_per_sample_grads, dim=0)) # average over samples
+            for idx, param in enumerate(self.net.parameters()):
+                param.grad = per_layer_mean_clipped_grads[idx]
+            # Now we are exactly in the same situation as in the non-DP case, i.e. with the mean batch gradients inside the
+            # parameters' grad attributes, with the exception that everything was clipped to ensure privacy
+        else:
+            closs.backward()
 
         return closs
 
@@ -191,22 +210,25 @@ class HamiltonianMonteCarlo:
         return potential_energy + kinetic_energy
 
     def __get_dp_energy(self, prop_q: list, prop_p: list, curr_q: list, curr_p: list, data_loader: torch.utils.data.DataLoader) -> torch.Tensor:
+        compute_per_sample_loss = vmap(self.hps.criterion, in_dims=(0, 0), out_dims=0)
         # save the current parameters
         start_params = self.net.get_params()
 
         batch_data, batch_target = self.__get_batch(data_loader)
         self.net.set_params(curr_q)
-        nll_initial = self.hps.criterion(self.net(batch_data), batch_target)
+        initial_per_sample_nll = compute_per_sample_loss(self.net(batch_data), batch_target)
         self.net.set_params(prop_q)
-        nll_proposal = self.hps.criterion(self.net(batch_data), batch_target)
-        ll_ratio_batch = nll_proposal - nll_initial # log of ratio of likelihoods: ln(p(x | theta') / p(x | theta))
+        proposal_per_sample_nll = compute_per_sample_loss(self.net(batch_data), batch_target)
 
-        # clip the log likelihood ratio for privacy in the acceptance step
+        # get the norm of the difference between the current and the proposal parameters, needed for the clip bound
         diff = torch.tensor([]).to(TORCH_DEVICE)
         for c_q, p_q in zip(curr_q, prop_q):
             diff = torch.cat((diff, torch.flatten(p_q - c_q)))
-        ll_ratio_clipped_bound = self.hps.acceptance_clip_bound * torch.norm(diff)
-        clipped_ll_ratio_batch = self.__clip(ll_ratio_batch, ll_ratio_clipped_bound)
+
+        #* compute the clipping bound, which is ||theta_prop - theta_curr||_2 * b_l, then use a vmap to clip the ratio for every sample
+        clipping_bound_ll_ratio = self.hps.acceptance_clip_bound * torch.norm(diff)
+        per_sample_clipped_ll_ratio = vmap(lambda nom, denom: self.__clip(nom / denom, clipping_bound_ll_ratio), in_dims=(0, 0), out_dims=0)
+        clipped_ll_ratio_batch = torch.mean(per_sample_clipped_ll_ratio(proposal_per_sample_nll, initial_per_sample_nll))
 
         # compute the kinetic energy
         kinetic_energy_init = torch.tensor(0.0).to(TORCH_DEVICE)
@@ -224,7 +246,7 @@ class HamiltonianMonteCarlo:
         prior_ratio_batch = proposal_prior_prob - init_prior_prob
 
         # noise term
-        dp_sigma_acceptance = 2 * self.hps.tau_l * ll_ratio_clipped_bound
+        dp_sigma_acceptance = 2 * self.hps.tau_l * clipping_bound_ll_ratio
         psi = dist.Normal(0, dp_sigma_acceptance).sample().to(TORCH_DEVICE)
 
         # correction term
@@ -237,19 +259,16 @@ class HamiltonianMonteCarlo:
         self.net.set_params(start_params)
         return torch.exp(energy_delta_corrected) # not to have the acceptance probability in log space
 
-    def __get_dp_grads(self, ll_grad: torch.Tensor) -> torch.Tensor:
-        dp_grad = copy.deepcopy(ll_grad)
-        if self.hps.run_dp:
-            # clip the gradient norm (first term) and add noise (second term)
-            dp_grad = self.__clip(dp_grad, self.hps.grad_clip_bound)
-            dp_noise = dist.Normal(0, 2 * self.hps.tau_g * self.hps.grad_clip_bound).sample(dp_grad.shape).to(TORCH_DEVICE)
-            # add the mean noise across the batch to grad_U
-            dp_grad += dp_noise / self.hps.batch_size
-
-        return dp_grad
-
     def __clip(self, x: torch.Tensor, bound: float) -> torch.Tensor:
-        return x * min(1, bound / torch.norm(x))
+        return x * torch.min(torch.tensor(1), bound / torch.norm(x))
+
+    def __compute_per_sample_loss(self, params: torch.Tensor, buffers: torch.Tensor, sample: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+        batch = sample.unsqueeze(0)
+        targets = target.unsqueeze(0)
+
+        predictions = functional_call(self.net, (params, buffers), (batch,))
+        loss = self.hps.criterion(predictions, targets)
+        return loss
 
     def __get_batch(self, data_loader: torch.utils.data.DataLoader) -> Tuple[torch.Tensor, torch.Tensor]:
         data = next(iter(data_loader))

probabilistic/HMC/uncertainty.py
@@ -36,7 +36,7 @@ def auroc(net: VanillaBnnLinear, test_set: Dataset, posterior_samples: List[torc
     assert np.isclose(auc_val.item(), alt_auroc, atol=1e-5)
     # ----------------------------------------------------------
 
-    return auc_val
+    return round(float(auc_val), 4)
 
 def ece(net: VanillaBnnLinear, test_set: Dataset, posterior_samples: List[torch.Tensor]) -> float:
     net.eval()
@@ -49,7 +49,7 @@ def ece(net: VanillaBnnLinear, test_set: Dataset, posterior_samples: List[torch.
     mc_ece = MulticlassCalibrationError(num_classes=10, n_bins=10, norm='l1')
     ece_val = mc_ece(mean_posterior_predictive_distribution, copy.deepcopy(test_set.targets).to(TORCH_DEVICE))
 
-    return ece_val
+    return round(float(ece_val), 4)
 
 def ibp_auc_and_ece(net: VanillaBnnLinear, test_set: Dataset, posterior_samples: List[torch.Tensor], eps: float) -> Tuple[float]:
     net.eval()