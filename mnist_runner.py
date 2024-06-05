48f54448677613b3aaf0eaa0118d5cd10edbc4fd
Mihnea Ghitu <mg2720@ada.doc.ic.ac.uk>
2024-06-05 21:21:25 +0100

    Add draft results for privacy study

--- mnist_runner.py
+++ mnist_runner.py
@@ -11,7 +11,8 @@ from experiments.experiment_utils import (compute_metrics_hmc,
                                           run_bnn_membership_inference_attack,
                                           run_experiment_adv_hmc,
                                           run_experiment_hmc,
-                                          run_experiment_sgd)
+                                          run_experiment_sgd,
+                                          save_samples)
 from globals import TORCH_DEVICE
 from probabilistic.HMC.adv_robust_dp_hmc import AdvHamiltonianMonteCarlo
 from probabilistic.HMC.attacks import ibp_eval
@@ -81,32 +82,39 @@ def privacy_study():
     # Run the ablation study
     conv_net = VanillaBnnMnist().to(TORCH_DEVICE)
     hyperparams = HyperparamsHMC(
-        num_epochs=1, num_burnin_epochs=0, step_size=0.01, lf_steps=3, batch_size=20000, num_chains=3, decay_epoch_start=50,
+        num_epochs=1, num_burnin_epochs=0, step_size=0.01, lf_steps=10, batch_size=6000, num_chains=3, decay_epoch_start=50,
         lr_decay_magnitude=0.5, warmup_step_size=0.2, momentum_std=0.001, prior_mu=0.0, prior_std=15, alpha_warmup_epochs=0,
         eps_warmup_epochs=0, alpha=0.993, alpha_pre_trained=0.75, step_size_pre_trained=0.001, eps=0.075,
-        run_dp=True, grad_clip_bound=0.05, acceptance_clip_bound=0.05, tau_g=2, tau_l=2
+        run_dp=True, grad_clip_bound=0.05, acceptance_clip_bound=0.05, tau_g=4, tau_l=4
     )
 
     testing_eps = 0.075
-    num_epochs = [1, 3 , 5, 7, 9, 11, 15, 19, 25, 30]
-    result_dict = {"eps": [], "dp": []}
-    for k in num_epochs:
+    num_epochs = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21]
+    dirnames = []
+    result_dict = {"num_epochs": []}
+    save_dir = __file__.rsplit('/', 1)[0] + "experiments/privacy_study/"
+    for k in num_epochs: 
+        dirnames.append(save_dir + f"k{k}/")
+    for k, dirname in zip(num_epochs, dirnames):
         # update hps
         hyperparams.num_epochs = k
+        if k >= 3: 
+            hyperparams.num_burnin_epochs = hyperparams.eps_warmup_epochs = hyperparams.alpha_warmup_epochs = 2 * k // 3
         hmc, posterior_samples = run_experiment_adv_hmc(conv_net, TRAIN_DATA, hyperparams, AttackType.IBP, init_from_trained=True)
+        save_samples(posterior_samples, dirname)
         hyperparams.eps = testing_eps
         std_acc = hmc.test_hmc_with_average_logits(TEST_DATA, posterior_samples)
         ibp_acc = ibp_eval(conv_net, hyperparams, TEST_DATA, posterior_samples)
         id_auroc = auroc(conv_net, TEST_DATA, posterior_samples)
         ood_auroc = ood_detection_auc_and_ece(conv_net, TEST_DATA, ood_data_test, posterior_samples)[0]
-        result_dict["eps"].append({"k": k,
+        result_dict["num_epochs"].append({"k": k,
                                    "std_acc": std_acc,
                                    "ibp_acc": ibp_acc,
                                    "id_auroc": id_auroc,
                                    "ood_auroc": ood_auroc})
-        print(f"Finished for epsilon: {k}")
+        print(f"Finished for k={k}")
 
-    with open("experiments/ablation_mnist.yaml", "w", encoding="utf-8") as f:
+    with open("experiments/privacy_study.yaml", "w", encoding="utf-8") as f:
         yaml.dump(result_dict, f)
 
 def ablation_study():
@@ -154,7 +162,7 @@ def ablation_study():
                                   "id_auroc": id_auroc,
                                   "ood_auroc": ood_auroc})
         print(f"Finished for clip bound: {clip_bound}")
-    with open("experiments/privacy_mnist.yaml", "w", encoding="utf-8") as f:
+    with open("experiments/ablation_mnist.yaml", "w", encoding="utf-8") as f:
         yaml.dump(result_dict, f)
 
 def membership_inference_bnn_experiment(adv: False, load_from_file: False):
@@ -181,3 +189,4 @@ TRAIN_DATA, TEST_DATA = load_mnist()
 # hmc_dp_experiment(write_results=False, for_adv_comparison=False, save_model=False)
 # dnn_experiment(save_model=False, write_results=False, for_adv_comparison=False)
 # ablation_study()
+privacy_study()
